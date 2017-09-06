bbc539976b855a793b82481abea4e32606247d40
JoelLindow <joellindow@gmail.com>
2017-09-06 17:43:36 -0600

    edit and update work. also included flash messages for layout view in future

--- spec/features/users/user_can_edit_user_profile_spec.rb
+++ spec/features/users/user_can_edit_user_profile_spec.rb
@@ -4,7 +4,7 @@ describe "User edits an existing company" do
   scenario "a user can edit a company" do
     user = User.create!(username: "User_1", email: 'sam1@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
 
-    visit user_path(user)
+    visit edit_user_path(user)
 
     fill_in "user[username]", with: "User_Elite"
     click_button "Update"
