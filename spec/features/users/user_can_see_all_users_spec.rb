6b49ff860325cabf1039a8423ceb9469d474f4b7
JoelLindow <joellindow@gmail.com>
2017-09-06 17:08:21 -0600

    test passing. user info displays to users/index.html.erb page at /users

--- spec/features/users/user_can_see_all_users_spec.rb
+++ spec/features/users/user_can_see_all_users_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 describe "User creates a new user profile" do
   scenario "a user can create a new user profile" do
 
-    User.new(username: "User_1", email: 'sam@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
-    User.new(username: "User_2", email: 'sam@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
-    User.new(username: "User_3", email: 'sam@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
-    User.new(username: "User_4", email: 'sam@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
-    User.new(username: "User_5", email: 'sam@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
+    User.create!(username: "User_1", email: 'sam1@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
+    User.create!(username: "User_2", email: 'sam2@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
+    User.create!(username: "User_3", email: 'sam3@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
+    User.create!(username: "User_4", email: 'sam4@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
+    User.create!(username: "User_5", email: 'sam5@sam.com', password: 'sam123', avatar_url: 'foo.png', about: "This is a sample about me snippet.", cover_letter: "This is a long letter.")
 
     visit '/users'
 
+    save_and_open_page
     expect(page).to have_content("User_1")
     expect(page).to have_content("User_2")
     expect(page).to have_content("User_3")
