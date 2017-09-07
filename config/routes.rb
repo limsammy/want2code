d7b6ba4496a3d9ad38556102e2b8dfbf1b3b8dcc
Sam Lim <limsammy1@gmail.com>
2017-09-07 13:44:18 -0600

    Pushing up for a second so joel can start on view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :users, only: [:new, :create] do
     resources :skills, only: [:new, :create, :show]
   end
+
+  root to: "home#index"
+  get '/login' to: "sessions#new"
+  post '/login' to: "sessions#create"
 end
