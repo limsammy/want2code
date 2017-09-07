360a31083837f84cc612f6a9b239b1ae3bbeaef7
Sam Lim <limsammy1@gmail.com>
2017-09-07 14:25:38 -0600

    Added logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
+  delete '/logout',  to: 'sessions#destroy'
 end
