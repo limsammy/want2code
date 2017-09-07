03725a083169dda3881de9623e10b53962ffb799
Sam Lim <limsammy1@gmail.com>
2017-09-07 13:53:40 -0600

    Assigned session and added current_user method to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:message] = "#{@user.username}'s profile has been created"
       @user.skills.create
       redirect_to new_user_skill_path(@user)
