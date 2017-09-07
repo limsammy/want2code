d1d0c46a4c3a312eb642fe7b82dd84c2e94546bf
Sam Lim <limsammy1@gmail.com>
2017-09-07 12:37:45 -0600

    Schema updated for first name and last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906210534) do
+ActiveRecord::Schema.define(version: 20170907183616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20170906210534) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
   add_foreign_key "friendships", "users"
