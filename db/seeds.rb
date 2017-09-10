943b5f233f02dbfbd9e2802f765d6615b8aeb4fd
Sam Lim <limsammy1@gmail.com>
2017-09-09 18:31:40 -0600

    Rewrote seed file to include skills for about graph

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,13 @@ sleep(0.15)
 print '.'
 
 100.times do |i|
-  User.create!(username: Faker::Internet.user_name, email: Faker::Internet.email, password: Faker::Internet.password,
-    avatar_url: Faker::Avatar.image, about: Faker::Lorem.sentence(3),
-    cover_letter: Faker::Lorem.sentence(6), role: 0, created_at: rand(1.years).seconds.ago)
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+    username: Faker::Internet.unique.user_name, email: Faker::Internet.unique.email, password: Faker::Internet.password,
+    avatar_url: Faker::Avatar.unique.image, about: Faker::Lorem.sentence(3),
+    cover_letter: Faker::Lorem.sentence(6), quote: Faker::Movie.quote, role: 0, created_at: rand(1.years).seconds.ago)
   puts "Created #{i} users!"
 
+  rand(1..10).times do |i|
+    user.skills.create(name: Faker::Job.key_skill, level: 'Advanced', created_at: rand(1.years).seconds.ago)
+  end
 end
